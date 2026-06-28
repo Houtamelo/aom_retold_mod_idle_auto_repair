4765dfbf974e9e5c2b1ec6ae123d2c48ffef0220
houtamelo <antoniopedrogf@hotmail.com>
2026-06-28 21:15:53 +0000

    Revert "fix(xs-plugin): lowercase XsLanguage id and plugin.xml references"
    This reverts commit fd6b80f6d1f61be43c5fc0373f9dca26dc260399.

--- tools/intellij-xs-plugin/src/main/kotlin/com/aomr/xs/XsLanguage.kt
+++ tools/intellij-xs-plugin/src/main/kotlin/com/aomr/xs/XsLanguage.kt
@@ -2,7 +2,7 @@ package com.aomr.xs
 
 import com.intellij.lang.Language
 
-class XsLanguage private constructor() : Language("xs") {
+class XsLanguage private constructor() : Language("XS") {
     companion object {
         val INSTANCE = XsLanguage()
     }
